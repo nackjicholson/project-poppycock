613e87c5678cadf0b83b482239e3b5bf72684f3e
Willie V <willieviseoae@gmail.com>
2012-08-07 23:50:34 -0700

    omfg

---
+++ wordpress/wp-content/themes/poppycock/view/category-images-ii/term-images.php
@@ -0,0 +1,21 @@
+<?php if (!defined ('ABSPATH')) die ('No direct access allowed'); ?>
+
+<ul class="category_images_ii term-images taxonomy-<?php echo esc_attr( $taxonomy ); ?>">
+<?php foreach( $terms AS & $term ) { ?>
+	<li class="category_image term_image">
+	<?php if ( $link_images ) : ?>
+		<a href="<?php echo esc_attr( get_term_link( $term[ 'id' ], $taxonomy ) ); ?>">
+	<?php endif; ?>
+			<img src="<?php echo $term[ 'image' ]; ?>" alt="<?php echo $term[ 'name' ]; ?>" />
+	<?php if ( $link_images ) : ?>
+		</a>
+	<?php endif; ?>
+	<?php if ( $show_description ) : ?>
+		<a href="<?php echo esc_attr( get_term_link( $term[ 'id' ], $taxonomy ) ); ?>">
+			<?php echo $term['name']; ?>
+		</a>
+		<p><?php echo term_description( $term[ 'id' ], $taxonomy ); ?></p>
+	<?php endif; ?>
+	</li>
+<?php } // end for loop ?>
+</ul>
