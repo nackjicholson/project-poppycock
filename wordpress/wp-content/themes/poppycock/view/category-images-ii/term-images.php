b79a692e48dafe5f2774792b0759bdee05d00902
Willie V <willieviseoae@gmail.com>
2012-08-12 11:13:53 -0700

    sidebar home page styles for the categories

--- wordpress/wp-content/themes/poppycock/view/category-images-ii/term-images.php
+++ wordpress/wp-content/themes/poppycock/view/category-images-ii/term-images.php
@@ -1,5 +1,7 @@
 <?php if (!defined ('ABSPATH')) die ('No direct access allowed'); ?>
 
+<div class="categories-list fourcol last">
+<p class="h3">Post Categories</p>
 <ul class="category_images_ii term-images taxonomy-<?php echo esc_attr( $taxonomy ); ?>">
 <?php foreach( $terms AS & $term ) { ?>
 	<li class="category_image term_image">
@@ -11,9 +13,10 @@
 		</a>
 	<?php endif; ?>
 	<?php if ( $show_description ) : ?>
-		<a href="<?php echo esc_attr( get_term_link( $term[ 'id' ], $taxonomy ) ); ?>"><?php echo $term['name']; ?></a>
+		<a class="category_title" href="<?php echo esc_attr( get_term_link( $term[ 'id' ], $taxonomy ) ); ?>"><?php echo $term['name']; ?></a>
 		<p><?php echo term_description( $term[ 'id' ], $taxonomy ); ?></p>
 	<?php endif; ?>
 	</li>
 <?php } // end for loop ?>
 </ul>
+</div>
