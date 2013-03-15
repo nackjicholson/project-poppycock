527233368cfe2884409dd556bef8483a90b885a1
Willie V <willieviseoae@gmail.com>
2013-03-15 14:28:05 -0700

    Added an rss feed link and icon. It requires setup of theme menu.

--- wp-content/themes/poppycock/library/bones.php
+++ wp-content/themes/poppycock/library/bones.php
@@ -231,11 +231,12 @@ MENUS & NAVIGATION
 function bones_main_nav() {
 	// display the wp3 menu if available
     wp_nav_menu(array( 
-    	'container' => false,                           // remove nav container
+    	'container' => 'div',                           // remove nav container
     	'container_class' => 'menu clearfix',           // class of container (should you choose to use it)
     	'menu' => 'The Main Menu',                           // nav name
-    	'menu_class' => 'nav top-nav clearfix',         // adding custom nav class
+    	'menu_class' => 'nav main-nav clearfix',         // adding custom nav class
     	'theme_location' => 'main-nav',                 // where it's located in the theme
+      'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s<li class="rss-icon"><a target="_blank" href="/feed"></a></li></ul>',
     	'before' => '',                                 // before the menu
         'after' => '',                                  // after the menu
         'link_before' => '',                            // before each link
